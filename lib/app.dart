270945dddcba91e75d7ada0e12e7a403cfc09613
mattandayo <mattandayo@gmail.com>
2020-10-26 21:20:11 +0900

    Add main screen widget

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,53 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 enum CounterEvent { increment }
 
 class CounterBloc extends Bloc<CounterEvent, int> {
   @override
   int get initialState => 0;
-  
+
   @override
   Stream<int> mapEventToState(CounterEvent event) async* {
     switch (event) {
-      case CounterEvent.increment
+      case CounterEvent.increment:
         yield currentState + 1;
-      break;
+        break;
     }
+  }
+}
+
+class CounterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter')),
+      body: BlocBuilder<CounterBloc, int>(
+        builder: (context, count) => Center(
+          child: Text(
+            '$count',
+            style: TextStyle(fontSize: 24.0),
+          ),
+        ),
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                counterBloc.dispatch(CounterEvent.increment);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
